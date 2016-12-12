e51eef592b4966853f9a619f6613df657d67407a
aszanto9 <aszanto@college.harvard.edu>
2016-12-12 12:08:02 -0500

    fixed bug by updating size parameter to optional

--- algs.py
+++ algs.py
@@ -1,19 +1,30 @@
 import node_util
 
+"""allows for a very (very) pretty abstraction, wherein the implementation
+of graph search doesn't change, the user just decides which fringe type to
+use. Stack will run DFS, Queue will run BFS, priority queues will run either UCS
+or A*, depending on the cost function. Yay!"""
+
 
 class Fringe:
+
     def __init__(self, s):
         self.structure = s()
         assert ('push' in dir(s) and 'pop' in dir(s) and 'isEmpty' in dir(s))
 
     def push(self, item, base, cost):
-        self.structure.push(item) if cost == 0 else self.structure.push(item, base + cost)
+        self.structure.push(item) if cost == 0 else self.structure.push(
+            item, base + cost)
 
     def pop(self):
         return self.structure.pop()
 
     def isEmpty(self):
         return self.structure.isEmpty()
+
+"""fringe-agnostic graph search problem. Inputs are the type of fringe, number of
+pipes to solve, and cost function for a node (constant for UCS,
+heuristic for A*)"""
 
 
 def search(structure, num_pipes, cost_function=None):
@@ -27,8 +38,10 @@ def search(structure, num_pipes, cost_function=None):
     visited[start.state] = 0
 
     for successor in node_util.getSuccessors(start.state):
-        fringe.push((successor.state, [successor.flapped]), successor.cost, cost_function(successor))
-        visited[successor.state] = successor.cost  # _update best cost to successors
+        fringe.push((successor.state, [successor.flapped]),
+                    successor.cost, cost_function(successor))
+        # _update best cost to successors
+        visited[successor.state] = successor.cost
     called = 0
     while not fringe.isEmpty():
         called += 1
